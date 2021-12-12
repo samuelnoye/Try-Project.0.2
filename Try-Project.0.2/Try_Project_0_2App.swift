ec9b86d6cb54df92e7ed14b2c8e2355a2637d48b
Samuel Noye <samuel.noye@amalitech.org>
2021-12-12 10:57:18 +0000

    Initial Commit

---
+++ Try-Project.0.2/Try_Project_0_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Try_Project_0_2App.swift
+//  Try-Project.0.2
+//
+//  Created by Samuel Noye on 12/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Try_Project_0_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
